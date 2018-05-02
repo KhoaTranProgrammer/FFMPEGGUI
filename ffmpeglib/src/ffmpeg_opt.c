bd1eb8e88869394f6d217917c3e7bd979c5bc553
Khoa Trần <khoatranprogrammer@gmail.com>
2018-05-02 14:25:22 +0700

    CMD: supports to print more options

--- ffmpeglib/src/ffmpeg_opt.c
+++ ffmpeglib/src/ffmpeg_opt.c
@@ -3204,28 +3204,28 @@ void show_help_default(const char *opt, const char *arg)
     show_help_options(options, "Global options (affect whole program "
                       "instead of just one file:",
                       0, per_file | OPT_EXIT | OPT_EXPERT, 0);
-//    if (show_advanced)
-//        show_help_options(options, "Advanced global options:", OPT_EXPERT,
-//                          per_file | OPT_EXIT, 0);
+    if (show_advanced)
+        show_help_options(options, "Advanced global options:", OPT_EXPERT,
+                          per_file | OPT_EXIT, 0);
 
     show_help_options(options, "Per-file main options:", 0,
                       OPT_EXPERT | OPT_AUDIO | OPT_VIDEO | OPT_SUBTITLE |
                       OPT_EXIT, per_file);
-//    if (show_advanced)
-//        show_help_options(options, "Advanced per-file options:",
-//                          OPT_EXPERT, OPT_AUDIO | OPT_VIDEO | OPT_SUBTITLE, per_file);
+    if (show_advanced)
+        show_help_options(options, "Advanced per-file options:",
+                          OPT_EXPERT, OPT_AUDIO | OPT_VIDEO | OPT_SUBTITLE, per_file);
 
     show_help_options(options, "Video options:",
                       OPT_VIDEO, OPT_EXPERT | OPT_AUDIO, 0);
-//    if (show_advanced)
-//        show_help_options(options, "Advanced Video options:",
-//                          OPT_EXPERT | OPT_VIDEO, OPT_AUDIO, 0);
+    if (show_advanced)
+        show_help_options(options, "Advanced Video options:",
+                          OPT_EXPERT | OPT_VIDEO, OPT_AUDIO, 0);
 
     show_help_options(options, "Audio options:",
                       OPT_AUDIO, OPT_EXPERT | OPT_VIDEO, 0);
-//    if (show_advanced)
-//        show_help_options(options, "Advanced Audio options:",
-//                          OPT_EXPERT | OPT_AUDIO, OPT_VIDEO, 0);
+    if (show_advanced)
+        show_help_options(options, "Advanced Audio options:",
+                          OPT_EXPERT | OPT_AUDIO, OPT_VIDEO, 0);
     show_help_options(options, "Subtitle options:",
                       OPT_SUBTITLE, 0, 0);
     av_log(NULL, AV_LOG_INFO, "\n");
@@ -3239,7 +3239,7 @@ void show_help_default(const char *opt, const char *arg)
 //#endif
 //        show_help_children(swr_get_class(), AV_OPT_FLAG_AUDIO_PARAM);
 //        show_help_children(avfilter_get_class(), AV_OPT_FLAG_VIDEO_PARAM | AV_OPT_FLAG_AUDIO_PARAM | AV_OPT_FLAG_FILTERING_PARAM);
-    }
+//    }
 }
 
 void show_usage(void)
