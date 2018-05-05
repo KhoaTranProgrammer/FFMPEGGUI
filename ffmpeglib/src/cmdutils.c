a4171da10c073df6a326d16ca15703dafd8e40b0
Khoa Trần <khoatranprogrammer@gmail.com>
2018-05-05 07:33:32 +0700

    CMD: supports show version

--- ffmpeglib/src/cmdutils.c
+++ ffmpeglib/src/cmdutils.c
@@ -1166,7 +1166,7 @@ void show_banner(int argc, char **argv, const OptionDef *options)
 
 int show_version(void *optctx, const char *opt, const char *arg)
 {
-    av_log_set_callback(log_callback_help);
+//    av_log_set_callback(log_callback_help);
     print_program_info (SHOW_COPYRIGHT, AV_LOG_INFO);
     print_all_libs_info(SHOW_VERSION, AV_LOG_INFO);
 
