a3a1c76aa34b9ae89c5c055d4d8cd13d9d626363
Khoa Trần <khoatranprogrammer@gmail.com>
2018-05-05 08:00:18 +0700

    CMD: supports to show build configuration

--- ffmpeglib/src/cmdutils.c
+++ ffmpeglib/src/cmdutils.c
@@ -1175,7 +1175,7 @@ int show_version(void *optctx, const char *opt, const char *arg)
 
 int show_buildconf(void *optctx, const char *opt, const char *arg)
 {
-    av_log_set_callback(log_callback_help);
+//    av_log_set_callback(log_callback_help);
     print_buildconf      (INDENT|0, AV_LOG_INFO);
 
     return 0;
