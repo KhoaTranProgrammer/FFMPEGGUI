f511492f32f8f0c3f91a4ccb25bc00893174a0b0
Khoa Trần <khoatranprogrammer@gmail.com>
2018-05-05 09:14:33 +0700

    CMD: supports to show available color names

--- ffmpeglib/src/cmdutils.c
+++ ffmpeglib/src/cmdutils.c
@@ -1681,10 +1681,10 @@ int show_colors(void *optctx, const char *opt, const char *arg)
     const uint8_t *rgb;
     int i;
 
-    printf("%-32s #RRGGBB\n", "name");
+    av_log(NULL, AV_LOG_INFO, "%-32s #RRGGBB\n", "name");
 
     for (i = 0; name = av_get_known_color_name(i, &rgb); i++)
-        printf("%-32s #%02x%02x%02x\n", name, rgb[0], rgb[1], rgb[2]);
+        av_log(NULL, AV_LOG_INFO, "%-32s #%02x%02x%02x\n", name, rgb[0], rgb[1], rgb[2]);
 
     return 0;
 }
